78b440a3cbcc9ea8e7af2cbbdc60baa13fc8e124
Thet Htoo Htoo San <thethtoohtoosan2172000@gmail.com>
2023-03-09 11:13:41 +0630

    add files

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.css';
+import Task from './components/task';
+import Input from './components/Input'
+import Parent from './components/ContextTest/TestContent';
+function App() {
+
+  const tasks = [
+    { id: 1, desc: 'go to gym' },
+    { id: 2, desc: 'go home' }
+  ]
+
+  return (
+    <div className="App">
+      {/* <div className="pj-wrap">
+        <div className="title"><h1>What's the Plan Today?</h1></div>
+        <div className="todo-body">
+          <form action="">
+            <div className="todo">
+              <Input placeholder={'add to do'} />
+              <button>ADD</button>
+            </div>
+            <div className="todo">
+              <Input placeholder={'update to do'} />
+
+              <button>UPDATE</button>
+            </div>
+          </form>
+
+          <div className="tasks">
+            {
+              tasks.map((item, index) => {
+                return <Task key={index} item={item} />
+              })
+            }
+          </div>
+        </div>
+      </div> */}
+      <Parent />
+    </div>
+  );
+}
+
+export default App;
